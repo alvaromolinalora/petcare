a66709a01534e8821b2d6c0680834fcad85c3a55
alvaromolinalora <alvaro.molina@a.vedrunasevillasje.es>
2025-11-26 13:55:20 +0100

    Añadir usuario demo a H2 en Render

---
+++ src/main/resources/data-render.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuarios (nombre_usuario, contrasena_hash, nombre_completo)
+VALUES (
+    'demo',
+    '$2b$10$91FAlnxpiTsDoK5EpVRNsObeT8LUPSj3ox4JrWjPbtKEdWyQP3rW2',
+    'Usuario Demo'
+);
